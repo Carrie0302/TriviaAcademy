5665f78e80cdfc3dcda0cc2a4e07129d352f07f3
Carrie <clipsong@gmail.com>
2019-04-16 11:45:58 -0700

    Question and QuestionBank implement Parceable so they can be passed as objects to Activities

--- app/src/main/java/com/example/triviaacademy/model/Question.java
+++ app/src/main/java/com/example/triviaacademy/model/Question.java
@@ -1,7 +1,11 @@
 package com.example.triviaacademy.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -10,7 +14,7 @@ import java.util.Random;
  * class Question
  * Creates a question with a list of choices and an index of the correct answer
  */
-public class Question {
+public class Question implements Parcelable {
 
     /**
      * Constructor Question with all choices
@@ -41,6 +45,60 @@ public class Question {
         choices.add(randomAns, correctAnswer);
         this.setChoiceList(choices);
         this.setAnswer(randomAns);
+    }
+
+    /**
+     * Allow Question to be parsed to another component
+     *
+     * @param in parcel
+     */
+    protected Question(Parcel in) {
+        mQuestion = in.readString();
+        this.mChoiceList = new ArrayList<>();
+        in.readList(this.mChoiceList, getClass().getClassLoader());
+        mAnswerIndex = in.readInt();
+    }
+
+    /**
+     * Generates instances of the Parcelable Question from a Parcel
+     * for passing across Activities
+     */
+    public static final Creator<Question> CREATOR = new Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation.
+     * @return 0 means no special object types marshaled
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Write all object members into parcel objects for parsing
+     *
+     * @param dest  the Parcel in which the object should be written.
+     * @param flags additional flags about how the object should be written.
+     *              May be 0 or PARCELABLE_WRITE_RETURN_VALUE. Value is either
+     *              0 or a combination of PARCELABLE_WRITE_RETURN_VALUE, and
+     *              android.os.Parcelable.PARCELABLE_ELIDE_DUPLICATES
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mQuestion);
+        dest.writeList(mChoiceList);
+        dest.writeInt(mAnswerIndex);
     }
 
     /**
--- app/src/main/java/com/example/triviaacademy/model/QuestionBank.java
+++ app/src/main/java/com/example/triviaacademy/model/QuestionBank.java
@@ -1,5 +1,9 @@
 package com.example.triviaacademy.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +12,7 @@ import java.util.List;
  * class QuestionBank
  * Creates a bank of questions and answers that can be iterated through
  */
-public class QuestionBank {
+public class QuestionBank implements Parcelable {
 
     /**
      * Constructor for empty question list
@@ -28,6 +32,58 @@ public class QuestionBank {
         mQuestionList = new ArrayList<>();
         mQuestionList.addAll(questions);
         Collections.shuffle(mQuestionList);
+    }
+
+    /**
+     * Allow Question Bank to be parsed to another component
+     *
+     * @param in parcel
+     */
+    protected QuestionBank(Parcel in) {
+        this.mNextQuestionIndex = in.readInt();
+        this.mQuestionList = new ArrayList<>();
+        in.readList(this.mQuestionList, getClass().getClassLoader());
+    }
+
+    /**
+     * Generates instances of the Parcelable QuestionBank from a Parcel
+     * for passing across Activities
+     */
+    public static final Creator<QuestionBank> CREATOR = new Creator<QuestionBank>() {
+        @Override
+        public QuestionBank createFromParcel(Parcel in) {
+            return new QuestionBank(in);
+        }
+
+        @Override
+        public QuestionBank[] newArray(int size) {
+            return new QuestionBank[size];
+        }
+    };
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation.
+     * @return 0 means no special object types marshaled
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Write all object members into parcel objects for parsing
+     *
+     * @param dest  the Parcel in which the object should be written.
+     * @param flags additional flags about how the object should be written.
+     *              May be 0 or PARCELABLE_WRITE_RETURN_VALUE. Value is either
+     *              0 or a combination of PARCELABLE_WRITE_RETURN_VALUE, and
+     *              android.os.Parcelable.PARCELABLE_ELIDE_DUPLICATES
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mNextQuestionIndex);
+        dest.writeList(mQuestionList);
     }
 
     /**
